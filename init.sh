dddcb7c370199c847d89ec0032852c0d634f6950
Hernanda Achmad <hernandaislearning@gmail.com>
2025-09-23 20:54:43 +0700

    update: bash

---
+++ init.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install -y \
+    ros-noetic-tf2-ros \
+    ros-noetic-turtlebot3 \
+    ros-noetic-turtlebot3-simulations \
+    ros-noetic-turtlebot3-gazebo \
+    ros-noetic-gmapping \
+    ros-noetic-rviz \
+    ros-noetic-map-server \
+    ros-noetic-amcl \
+    ros-noetic-move-base \
+    ros-noetic-navigation
